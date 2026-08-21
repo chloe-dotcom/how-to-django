a25c1683004382fb19ae496ffe1e2c2df0cc1833
chloe <chloez@mit.edu>
2026-08-21 13:04:06 -0700

    explore database models, migrations, etc

--- demo/demo/myapp/models.py
+++ demo/demo/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# ORM: object relational mapping
+# - write python code to write database models
+# - connects OOP to databases by converting data automatically
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
